1b143dd81f6a22c21ae5ff6fb8a12badeceeb687
DhavalPithwa <pithwa.siv.ps2gmail.com>
2020-01-06 18:35:16 +0530

    api crud

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Employee;
+use Session;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Employee::all();
+
+        return Response()->json($data,200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $emp = new Employee;
+
+        $emp->email = $request->email;
+        $emp->password = $request->password;
+
+        $emp_save = $emp->save();
+
+        return response()->json($emp_save,200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $emp)
+    {
+        $email = $emp->input("email");
+        $pass = $emp->input("password");
+
+        $count = Employee::where('email', $email)
+          ->where('password', $pass)->get();
+
+          if(count($count) > 0)
+          {
+            Session::put('id',$email);
+            return Response()->json($count,200);
+          }
+          else
+          {
+            return Response()->json($count,404); 
+          }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Employee $employee)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Employee $employee)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Employee $employee)
+    {
+        //
+    }
+}
